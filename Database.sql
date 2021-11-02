64d9e557159765786acc62f123f55e0273e5c0e8
wander23 <57674214+wander23@users.noreply.github.com>
2021-11-02 19:54:31 +0700

    DB + Phan quyen
    xong

---
+++ Database.sql
@@ -0,0 +1,222 @@
+﻿CREATE DATABASE HTDCHO
+GO 
+USE HTDCHO
+GO
+--------------------------------------Create table ---------------------------------------
+create TABLE DoiTac
+(
+	MaSoThue VARCHAR(10) PRIMARY KEY,
+	TenDoitac NVARCHAR(50),
+	NguoiDaiDien NVARCHAR(50),
+	ThanhPho NVARCHAR(50),
+	Quan NVARCHAR(50),
+	SoChiNhanh INT,
+	SoLuongDonHangMoiNgay INT,
+	LoaiHangVanChuyen INT,
+	DiaChiKinhDoanh NVARCHAR(50),
+	SDT VARCHAR(50),
+	Email VARCHAR(50)
+)
+
+create TABLE HopDong
+(
+	MaHopDong VARCHAR(10) PRIMARY KEY,
+	MaSoThue VARCHAR(10) NOT NULL,
+	SoChiNhanhDangKy INT,
+	PhanTramHoaHong FLOAT,
+	ThoiGianHieuLuc DATE,
+	TinhTrang NVARCHAR(50),
+	CHECK (TinhTrang IN (N'Đã duyệt', N'Chờ duyệt'))
+)
+
+create TABLE DonHang
+(
+	MaDonHang VARCHAR(10) PRIMARY KEY,
+	TenDonHang NVARCHAR(50),
+	NgayDat DATE,
+	TinhTrang NVARCHAR(50),
+	KhuVuc NVARCHAR(50),
+	PhiVanChuyen INT,
+	PhiSanPham INT,
+	HinhThucThanhToan NVARCHAR(50),
+	DiaChiGiaoHang NVARCHAR(50),
+	MaKhachHang VARCHAR(10) NOT NULL,
+	MaSoThue VARCHAR(10) NOT NULL,
+	MaTaiXe VARCHAR(10),
+	CHECK (TinhTrang IN (N'Đã giao', N'Đang giao',N'Chờ'))
+)
+
+create TABLE KhachHang
+(
+	MaKhachHang VARCHAR(10) PRIMARY KEY,
+	HoTen NVARCHAR(50),
+	SDT VARCHAR(50),
+	DiaChi NVARCHAR(50),
+	Email VARCHAR(50)
+)
+
+CREATE TABLE TaiXe
+(
+	MaTaiXe VARCHAR(10) PRIMARY KEY,
+	HoTen NVARCHAR(50),
+	CMND VARCHAR(50) UNIQUE,
+	DienThoai VARCHAR(50),
+	BienSoXe VARCHAR(50),
+	DiaChi NVARCHAR(50),
+	KhuVucHoatDong NVARCHAR(50),
+	Email NVARCHAR(50),
+	STK NVARCHAR(50),
+	NganHang NVARCHAR(50)
+)
+
+CREATE TABLE ChiNhanh
+(
+	MaChiNhanh VARCHAR(10) PRIMARY KEY,
+	MaSoThue VARCHAR(10) NOT NULL,
+	TenChiNhanh NVARCHAR(50),
+	DiaChi NVARCHAR(50)
+)
+
+CREATE TABLE ThongBao
+(
+	MaThongBao VARCHAR(10) PRIMARY KEY,
+	NoiDung NVARCHAR(50),
+	MaSoThue VARCHAR(10) NOT NULL
+)
+
+CREATE TABLE NhanVien
+(
+	MaNhanVien VARCHAR(10) PRIMARY KEY,
+	HoTen NVARCHAR(50),
+	SDT VARCHAR(10),
+	DiaChi NVARCHAR(50),
+	Email VARCHAR(50)
+)
+
+CREATE TABLE Admins
+(
+	MaAdmin VARCHAR(10) PRIMARY KEY,
+	HoTen NVARCHAR(50),
+	SDT VARCHAR(10),
+	DiaChi NVARCHAR(50),
+	Email VARCHAR(50)
+)
+
+
+CREATE TABLE SanPham
+(
+	MaSP VARCHAR(10) PRIMARY KEY,
+	MaChiNhanh VARCHAR(10) NOT NULL,
+	SoLuongTon INT,
+	GiaCa INT,
+	TenSP VARCHAR(50)
+)
+
+CREATE TABLE DHSP
+(
+	MaSP VARCHAR(10) PRIMARY KEY,
+	MaDonHang VARCHAR(10) NOT NULL,
+	SoLuong INT,
+	Gia INT,
+)
+--------------------------------------Foreign key-----------------------------------------
+GO 
+ALTER TABLE dbo.DonHang
+ADD CONSTRAINT FK_DH_KH
+FOREIGN KEY (MaKhachHang)
+REFERENCES dbo.KhachHang(MaKhachHang)
+
+ALTER TABLE dbo.DonHang
+ADD CONSTRAINT FK_DH_DT
+FOREIGN KEY (MaSoThue)
+REFERENCES dbo.DoiTac(MaSoThue)
+
+ALTER TABLE dbo.DonHang
+ADD CONSTRAINT FK_DH_TX
+FOREIGN KEY (MaTaiXe)
+REFERENCES dbo.TaiXe(MaTaiXe)
+
+ALTER TABLE dbo.HopDong
+ADD CONSTRAINT FK_HD_DT
+FOREIGN KEY (MaSoThue)
+REFERENCES dbo.DoiTac(MaSoThue)
+
+ALTER TABLE dbo.ThongBao
+ADD CONSTRAINT FK_TB_DT
+FOREIGN KEY (MaSoThue)
+REFERENCES dbo.DoiTac(MaSoThue)
+
+ALTER TABLE dbo.SanPham
+ADD CONSTRAINT FK_SP_CN
+FOREIGN KEY (MaChiNhanh)
+REFERENCES dbo.ChiNhanh(MaChiNhanh)
+
+ALTER TABLE dbo.DHSP
+ADD CONSTRAINT FK_DHSP_DH
+FOREIGN KEY (MaDonHang)
+REFERENCES dbo.DonHang(MaDonHang)
+
+ALTER TABLE dbo.DHSP
+ADD CONSTRAINT FK_DHSP_SP
+FOREIGN KEY (MaSP)
+REFERENCES dbo.SanPham(MaSP)
+
+----------------------------------------Trigger-------------------------------------------
+-- Thêm, chỉnh chi nhánh. mỗi đối tác có ghi số lượng chi nhánh khi đăng ký
+GO 
+CREATE TRIGGER tg_ChiNhanhTong
+ON dbo.ChiNhanh FOR INSERT,UPDATE
+as
+BEGIN
+    DECLARE @sochinhanh INT
+
+	SELECT @sochinhanh = COUNT(dbo.ChiNhanh.MaChiNhanh) FROM dbo.ChiNhanh,Inserted 
+	WHERE Inserted.MaSoThue = dbo.ChiNhanh.MaSoThue GROUP BY dbo.ChiNhanh.MaSoThue
+
+	IF @sochinhanh > (SELECT SoChiNhanh FROM dbo.DoiTac,Inserted WHERE Inserted.MaSoThue = dbo.DoiTac.MaSoThue)
+	BEGIN
+		PRINT N'Đã đủ số chi nhánh, không thể thêm'
+		ROLLBACK TRAN
+		return
+	END
+END
+
+-- Mỗi sản phẩm có 1 số lượng nhất định, nếu thêm sửa số lượng mà vượt quá số hiện tại thì huỷ
+GO 
+CREATE TRIGGER tg_LuongDatHang
+ON dbo.DHSP FOR INSERT,UPDATE
+as
+BEGIN
+    DECLARE @sosanpham INT
+
+	SELECT @sosanpham = SUM(DHSP.SoLuong) FROM dbo.DHSP, Inserted WHERE Inserted.MaSP = DHSP.MaSP
+
+	IF @sosanpham>(SELECT SoLuongTon FROM dbo.SanPham,Inserted WHERE dbo.SanPham.masp = Inserted.masp)
+	BEGIN
+	    PRINT N'Không thể đặt thêm'
+		ROLLBACK TRAN
+		return
+	END
+END
+
+-- Mỗi đối tác có số lượng đơn giao nhất định cho mỗi ngày nếu đã đủ thì không giao thêm đơn nào nữa
+GO 
+CREATE TRIGGER tg_DonHangHangNgay
+ON dbo.DonHang FOR INSERT
+as
+BEGIN
+    DECLARE @soDH INT
+
+	SELECT @soDH = COUNT(dbo.DonHang.MaDonHang) FROM dbo.DonHang,Inserted 
+	WHERE Inserted.MaSoThue = dbo.DonHang.MaSoThue AND dbo.DonHang.ngaydat = GETDATE()
+
+	IF @soDH > (SELECT SoLuongDonHangMoiNgay FROM dbo.DoiTac, Inserted WHERE Inserted.MaSoThue = dbo.DoiTac.MaSoThue)
+	BEGIN
+	    PRINT N'Không thể đặt thêm đơn hàng vì đã vượt quá số lượng đơn của đối tác'
+		ROLLBACK TRAN
+		return
+	END
+END
+
+
+
---
+++ privilege.sql
@@ -0,0 +1,111 @@
+﻿--Tạo database role Quản trị viên - Đối tác - Khách Hàng - Tài xế
+USE [HTDCHO]
+GO
+CREATE ROLE [quantri]
+CREATE ROLE [doitac]
+CREATE ROLE [khachhang]
+CREATE ROLE [taixe]
+CREATE ROLE [nhanvien]
+GO 
+------------------------------------Cấp quyền cho quản trị viên-----------------------------------------
+ALTER AUTHORIZATION ON SCHEMA::[db_accessadmin] TO [quantri]
+GRANT DELETE ON [dbo].[Admins] TO [quantri]
+GRANT INSERT ON [dbo].[Admins] TO [quantri]
+GRANT SELECT ON [dbo].[Admins] TO [quantri]
+GRANT UPDATE ON [dbo].[Admins] TO [quantri]
+GRANT DELETE ON [dbo].[NhanVien] TO [quantri]
+GRANT INSERT ON [dbo].[NhanVien] TO [quantri]
+GRANT SELECT ON [dbo].[NhanVien] TO [quantri]
+GRANT UPDATE ON [dbo].[NhanVien] TO [quantri]
+GRANT SELECT ON [dbo].[TaiXe] TO [quantri]
+GRANT UPDATE ON [dbo].[TaiXe] TO [quantri]
+GRANT ALTER ON ROLE::[quantri] TO [quantri]
+GRANT SELECT ON [dbo].[KhachHang] TO [quantri]
+GRANT UPDATE ON [dbo].[KhachHang] TO [quantri]
+GRANT ALTER ON ROLE::[taixe] TO [quantri]
+GRANT ALTER ON ROLE::[nhanvien] TO [quantri]
+GRANT ALTER ON ROLE::[khachhang] TO [quantri]
+GRANT SELECT ON [dbo].[DoiTac] TO [quantri]
+GRANT UPDATE ON [dbo].[DoiTac] TO [quantri]
+GRANT ALTER ON ROLE::[doitac] TO [quantri]
+GRANT ALTER ANY USER TO [quantri]
+
+
+------------------------------------Cấp quyền cho Đối tác-----------------------------------------
+GRANT DELETE ON [dbo].[SanPham] TO [doitac]
+GRANT INSERT ON [dbo].[SanPham] TO [doitac]
+GRANT UPDATE ON [dbo].[SanPham] TO [doitac]
+GRANT UPDATE ON [dbo].[DonHang] ([TinhTrang]) TO [doitac]
+GRANT INSERT ON [dbo].[DoiTac] TO [doitac]
+GRANT SELECT ON [dbo].[DonHang] TO [doitac]
+GRANT DELETE ON [dbo].[ChiNhanh] TO [doitac]
+GRANT REFERENCES ON [dbo].[ChiNhanh] TO [doitac]
+GRANT UPDATE ON [dbo].[ChiNhanh] TO [doitac]
+GRANT INSERT ON [dbo].[HopDong] TO [doitac]
+GRANT UPDATE ON [dbo].[HopDong] TO [doitac]
+
+------------------------------------Cấp quyền cho Khách Hàng-----------------------------------------
+GRANT SELECT ON [dbo].[DoiTac] ([TenDoiTac]) TO [khachhang]
+GRANT SELECT ON [dbo].[DoiTac] ([NguoiDaiDien]) TO [khachhang]
+GRANT SELECT ON [dbo].[SanPham] ([GiaCa]) TO [khachhang]
+GRANT SELECT ON [dbo].[SanPham] ([TenSP]) TO [khachhang]
+GRANT SELECT ON [dbo].[SanPham] ([SoLuongTon]) TO [khachhang]
+GRANT SELECT ON [dbo].[DoiTac] ([SDT]) TO [khachhang]
+GRANT SELECT ON [dbo].[DoiTac] ([ThanhPho]) TO [khachhang]
+GRANT SELECT ON [dbo].[DonHang] ([TinhTrang]) TO [khachhang]
+GRANT SELECT ON [dbo].[SanPham] ([GiaCa]) TO [khachhang]
+GRANT SELECT ON [dbo].[DoiTac] ([DiaChiKinhDoanh]) TO [khachhang]
+GRANT SELECT ON [dbo].[DoiTac] ([Email]) TO [khachhang]
+GRANT SELECT ON [dbo].[DonHang] ([TenDonHang]) TO [khachhang]
+GRANT SELECT ON [dbo].[DoiTac] ([LoaiHangVanChuyen]) TO [khachhang]
+GRANT INSERT ON [dbo].[KhachHang] TO [khachhang]
+
+------------------------------------Cấp quyền cho Tài xế-----------------------------------------
+GRANT UPDATE ON [dbo].[DonHang] ([TinhTrang]) TO [taixe]
+GRANT INSERT ON [dbo].[TaiXe] TO [taixe]
+GRANT SELECT ON [dbo].[DonHang] TO [taixe]
+
+------------------------------------Cấp quyền cho Nhân viên-----------------------------------------
+GRANT SELECT ON [dbo].[DoiTac] ([MaSoThue]) TO [nhanvien]
+GRANT SELECT ON [dbo].[DoiTac] ([TenDoiTac]) TO [nhanvien]
+GRANT SELECT ON [dbo].[DoiTac] ([SDT]) TO [nhanvien]
+GRANT SELECT ON [dbo].[DoiTac] ([Email]) TO [nhanvien]
+GRANT SELECT ON [dbo].[HopDong] TO [nhanvien]
+
+GO
+
+--Với lần nhập đơn đăng ký, tạo 1 login và set cho 1 role.
+--VÍ DỤ: tạo 5 tài khoản tương ứng với 5 role
+
+--Tạo login
+exec sp_addlogin 'Khoa', 'hehe', 'HTDCHO'
+exec sp_addlogin 'HAnh', 'hehe', 'HTDCHO'
+exec sp_addlogin 'PAnh', 'hehe', 'HTDCHO'
+exec sp_addlogin 'Nam', 'hehe', 'HTDCHO'
+exec sp_addlogin 'Phat', 'hehe', 'HTDCHO'
+
+
+--Map tới tên user trong db
+GO
+CREATE USER [quantri1] FOR LOGIN [Khoa] WITH DEFAULT_SCHEMA=[db_securityadmin]
+ALTER ROLE [quantri] ADD MEMBER [quantri1]
+
+
+GO
+CREATE USER [doitac1] FOR LOGIN [HAnh]
+ALTER ROLE [doitac] ADD MEMBER [doitac1]
+
+
+GO
+CREATE USER [khachhang1] FOR LOGIN [PAnh]
+ALTER ROLE [khachhang] ADD MEMBER [khachhang1]
+
+
+GO
+CREATE USER [taixe1] FOR LOGIN [Nam]
+ALTER ROLE [taixe] ADD MEMBER [taixe1]
+
+
+GO
+CREATE USER [nhanvien1] FOR LOGIN [Phat]
+ALTER ROLE [nhanvien] ADD MEMBER [nhanvien1]
