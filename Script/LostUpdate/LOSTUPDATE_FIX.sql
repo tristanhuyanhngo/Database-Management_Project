9b25df4f79b77c9eb10be22d91ace19e011336b1
wander23 <57674214+wander23@users.noreply.github.com>
2021-12-16 14:31:09 +0700

    fix baocao + script

---
+++ Script/LostUpdate/LOSTUPDATE_FIX.sql
@@ -0,0 +1,38 @@
+﻿CREATE PROC SP_NhanDonHang_fix
+	@MaTaiXe varchar(10), @MaDonHang varchar(10)
+AS
+BEGIN TRAN
+	BEGIN TRY
+		IF NOT EXISTs(select * from TaiXe where MaTaiXe = @MaTaiXe)
+			BEGIN
+				PRINT N'Tài Xế' + CAST(@MaTaiXe AS VARCHAR(10)) + N' Không Tồn Tại'
+				ROLLBACK TRAN
+				RETURN 1
+			END
+		IF NOT EXISTS(select * from DonHang,TaiXe where DonHang.KhuVuc = Taixe.KhuVucHoatDong and Taixe.MaTaiXe = @MaTaiXe and TinhTrang = N'Chờ' and MaDonHang = @MaDonHang)
+			BEGIN
+				PRINT N'Đặt hàng không khả thi'
+				ROLLBACK TRAN
+				RETURN 1
+			END
+
+		WAITFOR DELAY '0:0:05'
+		if (exists(select * from donhang where madonhang = @MaDonHang and TinhTrang = N'Chờ'))
+		begin
+			update DonHang set TinhTrang = N'Đang Giao',MaTaiXe = @MaTaiXe where @MaDonHang = MaDonHang 
+		end
+		else
+		BEGIN
+				PRINT N'không khả thi'
+				ROLLBACK TRAN
+				RETURN 1
+		END
+
+	END TRY
+	BEGIN CATCH
+		PRINT N'LỖI HỆ THỐNG'
+		ROLLBACK TRAN
+		RETURN 1	
+	END CATCH
+
+COMMIT TRAN
--- Script/LostUpdate/Tran_1_LostUpdate.sql
+++ Script/LostUpdate/Tran_1_LostUpdate.sql
@@ -2,4 +2,16 @@
 select DonHang.* from DonHang,TaiXe where TinhTrang = N'Chờ' and KhuVuc = KhuVucHoatDong and TaiXe.MaTaiXe = '1'
 
 -- đặt đơn hàng
-DECLARE @RT INT--EXEC @RT = SP_NhanDonHang '1','1'EXEC @RT = SP_NhanDonHang_fix '1','99'IF @RT = 1	PRINT N'THÊM THẤT BẠI'ELSE	PRINT N'THÊM THÀNH CÔNG'-- xem lại đơn hàng xem đã đặt thành công hay chưaselect * from DonHang
+DECLARE @RT INT
+--EXEC @RT = SP_NhanDonHang '1','1'
+EXEC @RT = SP_NhanDonHang_fix '1','99'
+IF @RT = 1
+	PRINT N'THÊM THẤT BẠI'
+ELSE
+	PRINT N'THÊM THÀNH CÔNG'
+
+-- xem lại đơn hàng xem đã đặt thành công hay chưa
+select * from DonHang
+
+
+
