7fdca9bb9a1d0bc456862a8667baeeca673cc028
wander23 <57674214+wander23@users.noreply.github.com>
2021-12-18 10:28:03 +0700

    fix login
    đang fix

--- Web/controllers/login.controller.js
+++ Web/controllers/login.controller.js
@@ -1,12 +1,18 @@
 import loginModel from "../models/login.model.js";
 
 export function paging(req, res) {
-    res.render("login")
+  res.render("login");
 }
 
 export function login(req, res) {
-    loginModel.checkLogin(req.body,(e,data)=>{
-        if(!e)  
-        res.render("home",{id:data.id})
-    })
+  loginModel.checkLogin(req.body, (e, data) => {
+    if (data!=undefined && data!=null) {
+      res.locals.id = data.id;
+      res.redirect("/");
+    }
+    else
+    {
+        res.status(404).json({ errors:"error" });
+    }
+  });
 }
