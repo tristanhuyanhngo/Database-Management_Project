e32897cae149f05cdca5ee8ddf70a58724b57140
wander23 <57674214+wander23@users.noreply.github.com>
2021-12-18 17:17:32 +0700

    up

--- Web/components/drive/drive.controller.js
+++ Web/components/drive/drive.controller.js
@@ -1,19 +1,16 @@
 import driveModel from "./drive.model.js";
 
 export function paging(req, res) {
-    res.render("home")
+  res.render("home");
 }
 
 export function table(req, res) {
-    res.render("table")
+  driveModel.getOrder("Gia Lai",(e, data) => {
+      console.log(data)
+    res.render("table",{orders:data});
+  });
 }
 
 export function one(req, res) {
-    res.render("one")
+  res.render("one");
 }
-
-// export function login(req, res) {
-//     loginModel.checkLogin(req.query,(e,data)=>{
-//         res.render("home",{id:data.id})
-//     })
-// }
--- Web/components/drive/drive.model.js
+++ Web/components/drive/drive.model.js
@@ -1,4 +1,20 @@
 import db from "../../utils/db.js";
 
 export default {
+  async getOrder(area, result) {
+    try {
+      const pool = await db.conn;
+      const sqlstring =
+        "select * from donhang where KhuVuc = @varArea";
+      return await pool
+        .request()
+        .input("varArea", db.sql.NVarChar, area)
+        .query(sqlstring, (e, data2) => {
+          if (data2.recordset.length>0) result(null, data2.recordset);
+          else result(true, null);
+        });
+    } catch {
+      result(true, null);
+    }
+  }
 };
