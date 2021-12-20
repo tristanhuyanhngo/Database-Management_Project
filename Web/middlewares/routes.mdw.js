e579b2c143554799dd21fd0eac3d2ba8ff3a497b
Huy Anh Ngo <tristanhuyanhngo@gmail.com>
2021-12-20 16:53:40 +0700

    Home for Customer & Partner Commit
    Complete Home for Customer & Partner

--- Web/middlewares/routes.mdw.js
+++ Web/middlewares/routes.mdw.js
@@ -4,6 +4,8 @@ import loginRouter from '../components/login/login.route.js'
 import registerRouter from '../components/register/register.route.js'
 import homeRouter from '../components/home/home.route.js'
 import driveRouter from '../components/drive/drive.route.js'
+import customerRouter from '../components/customer/customer.route.js'
+import partnerRouter from '../components/partner/partner.route.js'
 
 import path from 'path';
 import { dirname } from 'path';
@@ -16,6 +18,8 @@ export default function (app) {
     app.use('/login',loginRouter)
     app.use('/register',registerRouter)
     app.use('/drive',driveRouter)
+    app.use('/customer', customerRouter);
+    app.use('/partner', partnerRouter);
     app.use('/public', express.static('public'));
     app.use(express.static(path.join(__dirname, 'public')))
 };
