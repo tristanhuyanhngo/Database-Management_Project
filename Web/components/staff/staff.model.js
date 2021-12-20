f452cbed9eea7c1b26adab773c4e923063567225
Phương Anh <63282765+nnpanh@users.noreply.github.com>
2021-12-20 21:52:57 +0700

    staff renew + review contract

--- Web/components/staff/staff.model.js
+++ Web/components/staff/staff.model.js
@@ -19,7 +19,7 @@ export default {
     try {
       const pool = await db.conn;
       const sqlstring =
-        "select * from HopDong where HopDong.ThoiGianHieuLuc < getdate()'";
+        "select * from HopDong where HopDong.ThoiGianHieuLuc < getdate()";
       return await pool.request().query(sqlstring, (e, data) => {
         if (data.recordset.length > 0) result(null, data.recordset);
         else result(true, null);
