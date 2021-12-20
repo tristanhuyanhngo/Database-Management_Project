5563cf2f640bb13a5dbb893377343aa51e8d9ba9
Phương Anh <63282765+nnpanh@users.noreply.github.com>
2021-12-20 23:35:24 +0700

    button function

--- Web/components/staff/staff.controller.js
+++ Web/components/staff/staff.controller.js
@@ -15,4 +15,17 @@ export function renewalContract(req, res) {
     staffModel.renewalContract((e, data) => {
         res.render("staff/views/contract_renew",{data})
     });
-  }
+}
+export function updateState(req, res) {
+    //nhận id của đơn hàng vào sửa trạng thái từ chờ duyệt => đã duyệt
+    console.log(req.query.idVal);
+    res.redirect('/staff/review_contract/');
+}
+
+export function updateDate(req, res) {
+    //nhận id, "dd/mm/yy" của đơn hàng vào sửa ngày
+    console.log(req.query.idVal);
+    console.log(req.query.retVal)
+    res.redirect('/staff/extend_contract/');
+}
+
--- Web/components/staff/staff.route.js
+++ Web/components/staff/staff.route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import {paging, pendingContract, renewalContract} from './staff.controller.js';
+import {paging, pendingContract, renewalContract, updateDate, updateState} from './staff.controller.js';
 
 const router = express.Router();
 
 router.get('/', paging);
 router.get('/review_contract',pendingContract);
 router.get('/extend_contract',renewalContract);
+
+router.get('/update',updateState);
+router.get('/update_time',updateDate)
 export default router;
