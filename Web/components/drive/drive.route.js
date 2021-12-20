c31d1d7f3641eb5079d62dbb5b094c338c55986a
Huy Anh Ngo <tristanhuyanhngo@gmail.com>
2021-12-20 16:25:57 +0700

    UI Commit
    Complete UI for Driver

--- Web/components/drive/drive.route.js
+++ Web/components/drive/drive.route.js
@@ -3,6 +3,9 @@ import {paging, order_list, order_one, received_list, received_one} from './driv
 const router = express.Router();
 
 router.get('/', paging);
+router.get('/home',function (req, res) {
+    res.render('drive/views/home');
+});
 
 //NOT RECEIVED ORDER LIST
 router.get('/order_list', order_list);
@@ -12,3 +15,4 @@ router.get('/order_list/:id', order_one);
 router.get('/received_list',received_list);
 router.get('/received_list/:id',received_one);
 export default router;
+
