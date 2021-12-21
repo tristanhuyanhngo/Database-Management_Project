921ebea370d520c86b52bdf11efa521117cf634f
wander23 <57674214+wander23@users.noreply.github.com>
2021-12-21 10:42:35 +0700

    done update

--- Web/components/staff/staff.controller.js
+++ Web/components/staff/staff.controller.js
@@ -1,44 +1,42 @@
 import staffModel from "./staff.model.js";
 
 export function paging(req, res) {
-  res.render("");
+  res.render("home/views/home");
 }
 
 export function pendingContract(req, res) {
   staffModel.pendingContract((e, data) => {
-
-      res.render("staff/views/contract_review",{data})
+    res.render("staff/views/contract_review", { data });
   });
 }
 
 export function renewalContract(req, res) {
-    staffModel.renewalContract((e, data) => {
-        res.render("staff/views/contract_renew",{data})
-    });
+  staffModel.renewalContract((e, data) => {
+    res.render("staff/views/contract_renew", { data });
+  });
 }
 export function updateState(req, res) {
-    //nhận id của đơn hàng vào sửa trạng thái từ chờ duyệt => đã duyệt
-    console.log(req.query.idVal);
-    res.redirect('/staff/review_contract/');
+  staffModel.updateState(req.query.idVal, (e, data) => {
+    res.redirect("/staff/review_contract/");
+  });
 }
 
 export function updateDate(req, res) {
-    //nhận id, "dd/mm/yy" của đơn hàng vào sửa ngày
-    console.log(req.query.idVal);
-    console.log(req.query.retVal)
-    res.redirect('/staff/extend_contract/');
+  staffModel.updateDate(req.query.idVal, req.query.retVal, (e, data) => {
+    res.redirect("/staff/extend_contract/");
+  });
 }
 
-export function mail(req,res) {
-    //Mã hợp đồng
-    const id=req.params.id;
-    //Lấy ra mã partner của hợp đồng
-    const partner="Hehe";
-    //Biến PA dùng để viết thông báo bên FE
-    const type=req.params.msg;
-    var msg;
-    if (type===1) msg="review and accepted";
-    else msg="extended";
+export function mail(req, res) {
+  //Mã hợp đồng
+  const id = req.params.id;
+  //Lấy ra mã partner của hợp đồng
+  const partner = "Hehe";
+  //Biến PA dùng để viết thông báo bên FE
+  const type = req.params.msg;
+  var msg;
+  if (type === 1) msg = "review and accepted";
+  else msg = "extended";
 
-    res.render('staff/views/mail',{msg, partner, id});
+  res.render("staff/views/mail", { msg, partner, id });
 }
--- Web/components/staff/staff.model.js
+++ Web/components/staff/staff.model.js
@@ -6,7 +6,16 @@ export default {
       const pool = await db.conn;
       const sqlstring =
         "select * from HopDong where HopDong.TinhTrang = N'Chờ duyệt'";
-      return await pool.request().query(sqlstring, (e, data) => {
+      return pool.request().query(sqlstring, (e, data) => {
+        for (let i = 0; i < data.recordset.length; i++) {
+          if (data.recordset[i].ThoiGianHieuLuc != null) {
+            let dd = data.recordset[i].ThoiGianHieuLuc.getDate();
+            let mm = data.recordset[i].ThoiGianHieuLuc.getMonth() + 1;
+            let yyyy = data.recordset[i].ThoiGianHieuLuc.getFullYear();
+            data.recordset[i].ThoiGianHieuLuc = dd + "-" + mm + "-" + yyyy;
+          }
+        }
+
         if (data.recordset.length > 0) result(null, data.recordset);
         else result(true, null);
       });
@@ -21,9 +30,52 @@ export default {
       const sqlstring =
         "select * from HopDong where HopDong.ThoiGianHieuLuc < getdate()";
       return await pool.request().query(sqlstring, (e, data) => {
+        for (let i = 0; i < data.recordset.length; i++) {
+          if (data.recordset[i].ThoiGianHieuLuc != null) {
+            let dd = data.recordset[i].ThoiGianHieuLuc.getDate();
+            let mm = data.recordset[i].ThoiGianHieuLuc.getMonth() + 1;
+            let yyyy = data.recordset[i].ThoiGianHieuLuc.getFullYear();
+            data.recordset[i].ThoiGianHieuLuc = dd + "-" + mm + "-" + yyyy;
+          }
+        }
         if (data.recordset.length > 0) result(null, data.recordset);
         else result(true, null);
       });
+    } catch {
+      result(true, null);
+    }
+  },
+
+  async updateState(id, result) {
+    try {
+      const pool = await db.conn;
+      const sqlstring =
+        "update hopdong set TinhTrang = N'Đã duyệt' where MaHopDong = @varID";
+      return pool
+        .request()
+        .input("varID", db.sql.Int, id)
+        .query(sqlstring, (e, data) => {
+          if (!e) result(null, "thành công");
+          else result(true, null);
+        });
+    } catch {
+      result(true, null);
+    }
+  },
+
+  async updateDate(id, date, result) {
+    try {
+      const pool = await db.conn;
+      const sqlstring =
+        "update hopdong set ThoiGianHieuLuc = @varDate where MaHopDong = @varID";
+      return pool
+        .request()
+        .input('varDate',db.sql.Date,date)
+        .input('varID',db.sql.Int,id)
+        .query(sqlstring, (e, data) => {
+          if (!e) result(null, "thành công");
+          else result(true, null);
+        });
     } catch {
       result(true, null);
     }
