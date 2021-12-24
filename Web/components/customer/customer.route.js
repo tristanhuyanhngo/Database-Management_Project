316be9b4ce30c3d8fe62be20cc880dcb36935d53
Huy Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2021-12-24 18:50:32 +0700

    Revert "Report Commit"
    This reverts commit 14c595ce1ec7802cf9817f8d9971929e55d81756.

--- Web/components/customer/customer.route.js
+++ Web/components/customer/customer.route.js
@@ -1,25 +1,14 @@
 import express from 'express';
 import customerModel from './customer.model.js'
-import store from "store";
 
 const router = express.Router();
 
 router.get('/',function (req, res) {
-    const info = store.get('user');
-    res.render('customer/views/home', {
-        info
-    });
+    res.render('customer/views/home');
 });
 
-router.get('/order_list/:id', async function (req, res) {
-    const CusID = req.params.id || 0;
-    console.log(CusID);
+router.get('/order_list', async function (req, res) {
     const orders = await customerModel.findOrdersByCustomerID(4);
-
-    if (orders === undefined) {
-        return res.redirect('/');
-    }
-
     const customer_name = await customerModel.findCustomerByID(4);
 
     res.render('customer/views/order_list', {
