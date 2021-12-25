5fefa5cd752a595390b1e46c39cffbcbeb452dc5
wander23 <57674214+wander23@users.noreply.github.com>
2021-12-25 23:41:13 +0700

    up

--- Nhom7/SCRIPT/19127097_NguyenNgocPhuongAnh/PhantomRead/Tran_1_PhanTom.sql
+++ Nhom7/SCRIPT/19127097_NguyenNgocPhuongAnh/PhantomRead/Tran_1_PhanTom.sql
@@ -3,8 +3,8 @@ go
 
 --Xem sản phẩm
 DECLARE @RT INT
---EXEC @RT = SP_XemSanPham '8'
-EXEC @RT = SP_XemSanPham_fix '8'
+EXEC @RT = SP_XemSanPham '8'
+--EXEC @RT = SP_XemSanPham_fix '8'
 IF @RT = 1
 	PRINT N'XEM THẤT BẠI'
 ELSE
